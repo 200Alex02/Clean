af72c2d3486bd4afc6d6efe96d7f00fbc418a2a9
aleksandrleykin <leykin.sa@mail.ru>
2024-03-17 10:22:02 +0300

    add const for sharedPref

--- feature/data/src/main/java/com/feature/data/data_source/local/LocalDataSource.kt
+++ feature/data/src/main/java/com/feature/data/data_source/local/LocalDataSource.kt
@@ -6,13 +6,17 @@ import com.feature.data.model.PlayerDto
 import com.google.gson.Gson
 import javax.inject.Inject
 
+
+private const val SHARED_PREFS_NAME = "player_prefs"
+private const val KEY_PLAYERS = "players"
+
 internal class LocalDataSource @Inject constructor(
     application: Application
 ) {
     private val sharedPreferences =
-        application.getSharedPreferences("player_prefs", Context.MODE_PRIVATE)
+        application.getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE)
     private val gson = Gson()
-    private val playersJson = sharedPreferences.getString("players", "")
+    private val playersJson = sharedPreferences.getString(KEY_PLAYERS, "")
 
     fun getPlayersFromCash(): List<PlayerDto> {
         return if (playersJson.isNullOrEmpty()) {
@@ -24,6 +28,6 @@ internal class LocalDataSource @Inject constructor(
 
     fun savePlayers(players: List<PlayerDto>) {
         val playersJson = gson.toJson(players)
-        sharedPreferences.edit().putString("players", playersJson).apply()
+        sharedPreferences.edit().putString(KEY_PLAYERS, playersJson).apply()
     }
 }
